94528026ef1619d4aaea5857a7d48a2667c1b7ee
MelpoJason <jasonchan9280@gmail.com>
2016-08-08 14:46:04 +0800

    「Jason」 change test table database structure

--- src/test/resources/ssm4j.sql
+++ src/test/resources/ssm4j.sql
@@ -6,13 +6,17 @@ GRANT ALL PRIVILEGES ON ssm4j.* TO 'ssm4j_admin'@'localhost' ;
 
 DROP TABLE IF EXISTS 'user';
 
-create table test_table
-(
-  _id INTEGER NOT NULL COMMENT 'user id',
-  _name varchar(30) NOT NULL COMMENT 'user name',
-  context VARCHAR(100) COMMENT 'user context'
+CREATE TABLE user (
+  _id INTEGER NOT NULL COMMENT 'id',
+  _name VARCHAR(30) NOT NULL COMMENT 'name',
+  _password VARCHAR(100) NOT NULL COMMENT 'password',
+  _email VARCHAR(100) NOT NULL COMMENT 'email',
+  content VARCHAR(200) COMMENT 'content'
 );
 
-ALTER TABLE test_table ADD CONSTRAINT PK_TEST_TABLE PRIMARY KEY (_id,_name);
+INSERT INTO user  (user._id,user._name,user._password, user._email, user.content)
+  value (45, 'Jason', '------','jasonchan9280@gmail.com','test data');
 
-INSERT INTO test_table tt (tt._id, tt._name) VALUES (01,'jason');
+SELECT  * from user t where t._id = 45;
+
+ALTER TABLE user add CONSTRAINT PK_test_table PRIMARY KEY (_id, _name ,_password, _email)
