2d66b294147a2a1fb27f92736c9a757f96299934
MelpoJason <jasonchan9280@gmail.com>
2016-08-08 15:12:55 +0800

    「Jason」 change table name

--- src/test/resources/ssm4j.sql
+++ src/test/resources/ssm4j.sql
@@ -4,9 +4,9 @@ CREATE USER 'ssm4j_admin'@'localhost' IDENTIFIED BY 'admin';
 
 GRANT ALL PRIVILEGES ON ssm4j.* TO 'ssm4j_admin'@'localhost' ;
 
-DROP TABLE IF EXISTS 'user';
+DROP TABLE IF EXISTS 'users';
 
-CREATE TABLE user (
+CREATE TABLE users (
   _id INTEGER NOT NULL COMMENT 'id',
   _name VARCHAR(30) NOT NULL COMMENT 'name',
   _password VARCHAR(100) NOT NULL COMMENT 'password',
@@ -14,9 +14,9 @@ CREATE TABLE user (
   content VARCHAR(200) COMMENT 'content'
 );
 
-INSERT INTO user  (user._id,user._name,user._password, user._email, user.content)
+INSERT INTO users  (users._id,users._name,users._password, users._email, users.content)
   value (45, 'Jason', '------','jasonchan9280@gmail.com','test data');
 
-SELECT  * from user t where t._id = 45;
+SELECT  * from users t where t._id = 45;
 
-ALTER TABLE user add CONSTRAINT PK_test_table PRIMARY KEY (_id, _name ,_password, _email)
+ALTER TABLE users add CONSTRAINT PK_test_table PRIMARY KEY (_id, _name ,_password, _email)
